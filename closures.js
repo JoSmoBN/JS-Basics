bacca3df07b9a86739a61ce89dce15ad50844fba
Joseph Nolan <josephbnolan@me.com>
2016-02-15 09:09:29 -0600

    Finished all problems

--- closures.js
+++ closures.js
@@ -190,15 +190,24 @@ fnCounter( function(){
 
 
 
-/*
-  Make the following code work
 
-  funcArray[0]() //0
-  funcArray[1]() //1
-  funcArray[2]() //2
-  funcArray[3]() //3
-  funcArray[4]() //4
-  funcArray[5]() //5
 
-  *Hint: Don't let this fool you. Break down what's really happening here.
-*/
+var funcArray = [];
+for (var i = 0; i <= 10; i++) {
+  (function(hat) {
+    funcArray.push(function() {
+      console.log(hat);
+    });
+  }) (i);
+}
+
+
+
+funcArray[0]();
+funcArray[1]();
+funcArray[2]();
+funcArray[3]();
+funcArray[4]();
+funcArray[5]();
+
+//*Hint: Don't let this fool you. Break down what's really happening here.
